086e6d06eba71956ff2a029494bc8259a3d5e2e8
Luluh <lulualmogbil@gmail.com>
2025-06-19 12:48:50 +0300

    function 101 lab solved by Luluh

---
+++ myfunction.py
@@ -0,0 +1,37 @@
+#Luluh Almogbil
+'''
+# LAB_FUNCTIONS_101
+
+## Create a function that takes 1 parameter of type int , 
+then it prints out the result formatted like the following patter (if we give it 5 for example):
+
+**Example Output for `5`**
+```
+5 4 3 2 1   
+4 3 2 1   
+3 2 1   
+2 1   
+1   
+```
+
+#### Document the newly created function. describe what it does, then print the documentation.
+'''
+
+
+#defining the function
+def caclulate(number):
+    while number > 0:
+        for i in range(number,0,-1):
+           print(i,end=" ")
+        
+
+
+        print()
+        number-=1
+    
+
+#call the function
+number = int(input("Please enter a number: "))
+caclulate(number)
+   
+        
